76f62899e22a4b87a93a42d58518f6dc37b41b85
Shivansh <ishivanshgoel@gmail.com>
2021-04-24 12:49:59 +0530

    add forms

--- CoviHelp/login.py
+++ CoviHelp/login.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-## all login views
+from .forms import OxygenUser, HospitalUser, PharmaUser
 
+## all login views
 def oxygenlogin(request):
-    return render(request, "public/login/oxygen.html")
+    if request.method == 'POST':
+        pass
+    else:
+        form=OxygenUser()
+        return render(request, "public/login/oxygen.html", {'form':form})
 
 def hospitalslogin(request):
-    return render(request, "public/login/hospitals.html")
+    if request.method == 'POST':
+        pass
+    else:
+        form=HospitalUser()
+        return render(request, "public/login/hospitals.html", {'form':form})
 
 def pharmalogin(request):
-    return render(request, "public/login/pharma.html")
+    if request.method == 'POST':
+        pass
+    else:
+        form=PharmaUser()
+        return render(request, "public/login/pharma.html", {'form':form})

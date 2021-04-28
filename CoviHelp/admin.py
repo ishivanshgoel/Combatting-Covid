49296c42ea1088d4b44c89b016adac748f4df055
Shivansh <ishivanshgoel@gmail.com>
2021-04-28 10:49:34 +0530

    admin.py: register plasma model

--- CoviHelp/admin.py
+++ CoviHelp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import UserInfo, Hospital, Oxygen, Pharma
+from .models import UserInfo, Hospital, Oxygen, Pharma, Plasma
 # Register your models here.
 
 admin.site.register(UserInfo)
 admin.site.register(Oxygen)
 admin.site.register(Hospital)
 admin.site.register(Pharma)
+admin.site.register(Plasma)

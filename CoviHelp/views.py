308d4aeeab1e135c50cbce0072d92203c947766a
AnikaJagati <60574348+AnikaJagati@users.noreply.github.com>
2021-05-04 00:22:31 +0530

    Update views.py

--- CoviHelp/views.py
+++ CoviHelp/views.py
@@ -8,10 +8,15 @@ def index(request):
     return render(request, "public/index.html")
     
 def oxygen(request):
-    return render(request, "public/oxygen.html")
+    st = Statesdata()
+    states = st.getStates()
+    return render(request, "public/oxygen.html", {'states': states})
+
 
 def hospitals(request):
     return render(request, "public/hospitals.html")
 
 def pharma(request):
-    return render(request, "public/pharma.html")
+    st = Statesdata()
+    states = st.getStates()
+    return render(request, "public/pharma.html", {'states': states})

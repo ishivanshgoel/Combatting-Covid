861a9525b29355f3794393367ba49661b782e0a1
Shivansh <ishivanshgoel@gmail.com>
2021-05-08 01:28:40 +0530

    complete pharma and plasma
    Co-authored-by: TestPrab <TestPrab@users.noreply.github.com>

--- CoviHelp/views.py
+++ CoviHelp/views.py
@@ -5,28 +5,69 @@ from .models import Oxygen, Pharma, Plasma, Hospital
 
 from .Helpers.Statesdata import Statesdata
 import json
-import json as simplejson
 
 # Create your views here.
 def index(request):
     return render(request, "public/index.html")
-    
+
+### send state and district wise
 def oxygen(request):
-    st = Statesdata()
-    states = st.getStates()
-    return render(request, "public/oxygen.html", {'states': states})
-
-
-def hospitals(request):
-    return render(request, "public/hospitals.html")
+    try:
+        state = request.GET.getlist('state')[0]
+        city = request.GET.getlist('city')[0]
+        resources=Oxygen.objects.filter(state=state,city=city)
+        return render(request, "public/oxygenView.html", {
+            "resources":resources
+        })
+    except:
+        st = Statesdata()
+        states = st.getStates()
+        return render(request, "public/oxygen.html", {'states': states})
 
 def pharma(request):
-    st = Statesdata()
-    states = st.getStates()
-    return render(request, "public/pharma.html", {'states': states})
+    try:
+        state = request.GET.getlist('state')[0]
+        city = request.GET.getlist('city')[0]
+        resources=Pharma.objects.filter(state=state,city=city)
+        drugs=[]
+        for med in resources:
+            med=med.available_drugs.strip("[]").split(",")
+            drugs.append(med)
+        print(drugs)
+        return render(request, "public/pharmaView.html", {
+            "cleaned_resources": zip(resources, drugs)
+        })
+    except:
+        st = Statesdata()
+        states = st.getStates()
+        return render(request, "public/pharma.html", {'states': states})
 
-def getdetails(request):
-    state= request.GET['cnt']
-    st = Statesdata()
-    data = st.getDistricts(state)  
-    return HttpResponse(simplejson.dumps(data),content_type='application/json')
+def hospitals(request):
+    try:
+        state = request.GET.getlist('state')[0]
+        city = request.GET.getlist('city')[0]
+        resources=Hospital.objects.filter(state=state,city=city)
+        return render(request, "public/oxygenView.html", {
+            "resources":resources
+        })
+    except:
+        st = Statesdata()
+        states = st.getStates()
+        return render(request, "public/hospitals.html", {'states': states})
+
+def plasma(request):
+    try:
+        state = request.GET.getlist('state')[0]
+        city = request.GET.getlist('city')[0]
+        resources=Plasma.objects.filter(state=state,city=city)
+        return render(request, "public/plasmaView.html", {
+            "resources":resources
+        })
+    except:
+        st = Statesdata()
+        states = st.getStates()
+        return render(request, "public/plasma.html", {'states': states})
+
+    
+
+

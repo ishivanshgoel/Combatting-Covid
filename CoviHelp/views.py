466b29a41543344a57fcc7f6477202b466d95f90
Shivansh <ishivanshgoel@gmail.com>
2021-05-08 13:42:14 +0530

    pharmaView.html: fix minor bugs

--- CoviHelp/views.py
+++ CoviHelp/views.py
@@ -32,6 +32,7 @@ def pharma(request):
         drugs=[]
         for med in resources:
             med=med.available_drugs.strip("[]").split(",")
+            print(med)
             drugs.append(med)
         return render(request, "public/pharmaView.html", {
             "cleaned_resources": zip(resources, drugs)

ea94edfea8aa9c63b7aaf8131d20fc82273d71d5
Shivansh <ishivanshgoel@gmail.com>
2021-05-08 23:08:14 +0530

    update heroku

--- covid/settings.py
+++ covid/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 

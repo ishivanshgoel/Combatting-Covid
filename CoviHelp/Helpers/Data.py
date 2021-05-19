cdea1fb88eedfdecafc11cda446b5390545ae214
Shivansh <ishivanshgoel@gmail.com>
2021-05-19 20:10:01 +0530

    Data.py: options for available beds

--- CoviHelp/Helpers/Data.py
+++ CoviHelp/Helpers/Data.py
@@ -4,3 +4,6 @@ class Data():
     def available_drugs(self):
         drugs = ["Tocilizumab", "Remdesivir","Favipiravir", "Fabiflu 200 MG"]
         return drugs
+    def bed_options(self):
+        beds = ["ICU","Ventilator", "Oxygen"]
+        return beds
